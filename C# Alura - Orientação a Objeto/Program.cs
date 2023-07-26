866ea42ba022b18f25a0e98213a634c69e8927d5
Thauan <thauanteixeira53@gmail.com>
2023-07-26 11:52:32 -0300

    att

--- C# Alura - Orientação a Objeto/Program.cs
+++ C# Alura - Orientação a Objeto/Program.cs
@@ -1,11 +1,14 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "Roxane";
 musica1.artista = "The Police";
-Console.WriteLine($"Nome da música {musica1.nome}");
-Console.WriteLine($"Artista {musica1.artista}");
+musica1.duracao = 273;
+musica1.disponivel = true;
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
-Console.WriteLine($"\nNome da música {musica2.nome}");
-Console.WriteLine($"Artista {musica2.artista}");
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
