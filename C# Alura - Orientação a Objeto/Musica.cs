c64807cf4caeaf8a3768e9de1cf59382fccf7b8d
Thauan <thauanteixeira53@gmail.com>
2023-08-04 13:34:50 -0300

    att

--- C# Alura - Orientação a Objeto/Musica.cs
+++ C# Alura - Orientação a Objeto/Musica.cs
@@ -1,16 +1,27 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;                   
-    public bool disponivel;
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }                 
+    public bool Disponivel{ get; set; }
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence a banda {Artista}";
+
+    //public Genero Genero { get; set; }
+
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"\nNome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        if(disponivel)
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if(Disponivel)
         {
             Console.WriteLine("Disponivel no plano.");
         }
--- C# Alura - Orientação a Objeto/Program.cs
+++ C# Alura - Orientação a Objeto/Program.cs
@@ -1,14 +1,55 @@
-﻿Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 273;
-musica1.disponivel = true;
+﻿Banda queen = new Banda("Queen");
 
-Musica musica2 = new Musica();
-musica2.nome = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
-musica2.disponivel = false;
+Album albumDoQueen = new Album("A night at the opera");
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
+albumDoQueen.ExibirMusicasDoAlbum();
+queen.ExibirDiscografia();
+
+
+/*
+Banda U2 = new Banda("U2");
+
+Album albumDoU2 = new Album("The Joshua Tree");
+
+Musica musica3 = new Musica(U2, "With Or Without You")
+{
+    Duracao = 247,
+    Disponivel = true,
+};
+
+musica3.Duracao = 247;
+
+albumDoU2.AdicionarMusica(musica3);
+
+albumDoU2.ExibirMusicasDoAlbum();
+musica3.ExibirFichaTecnica();
+
+U2.AdicionarAlbum(albumDoU2);
+U2.ExibirDiscografia();
+*/
+
+
+
+
+
+
+
+
