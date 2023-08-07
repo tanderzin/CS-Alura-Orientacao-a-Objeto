5b56d4ccc1423e050dc2b7aaef2bf5c52c97754a
Thauan <thauanteixeira53@gmail.com>
2023-08-07 16:37:48 -0300

    att

--- C# Alura - Orientação a Objeto/Program.cs
+++ C# Alura - Orientação a Objeto/Program.cs
@@ -1,4 +1,5 @@
-﻿Banda queen = new Banda("Queen");
+﻿/*
+Banda queen = new Banda("Queen");
 
 Album albumDoQueen = new Album("A night at the opera");
 
@@ -22,6 +23,7 @@ musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
 albumDoQueen.ExibirMusicasDoAlbum();
 queen.ExibirDiscografia();
+*/
 
 
 /*
@@ -46,10 +48,19 @@ U2.AdicionarAlbum(albumDoU2);
 U2.ExibirDiscografia();
 */
 
+Episodio ep1 = new(2, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
 
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
 
+Podcast podcast = new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
 
-
-
+podcast.ExibirDetalhes();
 
 
